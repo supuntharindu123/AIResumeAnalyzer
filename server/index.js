4ed314240d330f5de70aefceba7ef1c57e222aca
supuntharindu123 <supuntharindu1125@gmail.com>
2025-06-22 19:16:24 +0530

    Initialize backend and frontend

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "./utill/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
